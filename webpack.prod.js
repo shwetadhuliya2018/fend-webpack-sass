2a5d44e729c54657ad6922e0a3dbb77b53b2ca2e
Shweta Dhuliya <shwetadhuliya@Shwetas-MacBook-Air.local>
2020-06-27 03:58:13 -0700

    adds service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,8 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -30,5 +30,6 @@ module.exports = {
             filename: "./index.html",
         }),
         new MiniCssExtractPlugin({filename: '[name].css'})
+        new WorkboxPlugin.GenerateSW()
     ]
 }
